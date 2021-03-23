e96a18f1171e08cb5fff8673e8e29bcd4c9b8dff
paverrier <paulalexis.verrier@ynov.com>
2021-03-23 14:21:38 +0100

    added all exercices until map

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using VCubWatcher.Models;
+
+namespace VCubWatcher.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Carte()
+        {
+            var station = GetBikeStationListFromApi();
+            return View(station);
+        }
+        public IActionResult Favoris()
+        {
+            return View();
+        }
+        public IActionResult ListeStations()
+        {
+            var station = GetBikeStationListFromApi();
+            return View(station);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private static readonly HttpClient client = new HttpClient();
+        private static List<BikeStation> GetBikeStationListFromApi()
+        {
+            var stringTask = client.GetStringAsync("https://api.alexandredubois.com/vcub-backend/vcub.php");
+            var myJsonResponse = stringTask.Result;
+            var result = JsonConvert.DeserializeObject<List<BikeStation>>(myJsonResponse);
+            return result;
+        }
+    }
+}
